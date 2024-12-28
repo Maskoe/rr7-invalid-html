20d39e160d57a80a3d3cc29f084ea2588bb4124a
Mirco Benthien <Mirco_Benthien@hotmail.com>
2024-12-28 12:56:50 +0100

    workaround

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import parse from "html-react-parser";
 
 const brokenHtml = "<i> almost italic <i>";
 export function Welcome() {
   return (
     <main className="grid items-center justify-center pt-16 pb-4">
       <p dangerouslySetInnerHTML={{ __html: brokenHtml }} suppressHydrationWarning></p>
+      {/* <p>{parse(brokenHtml)}</p> */}
       <Link to="/other" className="border">
         navigate away
       </Link>
